e0d897618468a5ccafec331104cca824c9d8e3c8
Corné <corne@cp3.io>
2022-12-01 13:28:08 +0100

    simplified

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::time::Instant;
 
 fn day1_1() -> i32 {
     let contents = fs::read_to_string("./input").unwrap();
@@ -9,7 +8,7 @@ fn day1_1() -> i32 {
         .into_iter()
         .map(|e| e.split("\n").collect::<Vec<&str>>())
         .map(|e| e.into_iter().map(|f| f.parse::<i32>().unwrap_or(0)))
-        .map(|e| e.reduce(|a, b| a + b).unwrap_or(0))
+        .map(|e| e.sum())
         .collect();
     xs.sort();
 
@@ -24,28 +23,16 @@ fn day1_2() -> i32 {
         .into_iter()
         .map(|e| e.split("\n").collect::<Vec<&str>>())
         .map(|e| e.into_iter().map(|f| f.parse::<i32>().unwrap_or(0)))
-        .map(|e| e.reduce(|a, b| a + b).unwrap_or(0))
+        .map(|e| e.sum())
         .collect();
     xs.sort();
 
-    let top3_values = xs[(xs.len() - 3)..xs.len()].into_iter();
-    let mut result = 0;
-    for e in top3_values {
-        result += e;
-    }
-    result
+    xs[(xs.len() - 3)..xs.len()].into_iter().sum()
 }
 
 fn main() {
-    let start = Instant::now();
     day1_1();
-    let elapsed_time = start.elapsed().as_micros();
-    println!("day1_1 executed in {} microseconds", elapsed_time);
-
-    let start = Instant::now();
     day1_2();
-    let elapsed_time = start.elapsed().as_micros();
-    println!("day1_2 executed in {} microseconds", elapsed_time);
 }
 
 #[cfg(test)]
@@ -54,10 +41,7 @@ mod tests {
 
     #[test]
     fn test_day1_1() {
-        let start = Instant::now();
         let result = day1_1();
-        let elapsed_time = start.elapsed().as_micros();
-        println!("executed in {} microseconds", elapsed_time);
         assert_eq!(result, 72718);
     }
 
@@ -68,10 +52,7 @@ mod tests {
 
     #[test]
     fn test_day1_2() {
-        let start = Instant::now();
         let result = day1_2();
-        let elapsed_time = start.elapsed().as_micros();
-        println!("Executed in {} microseconds", elapsed_time);
         assert_eq!(result, 213089);
     }
 }
