0bbe11ff6e1c36b2cdc33440d31cc301caf44b0a
Corné <corne@cp3.io>
2022-12-02 12:55:13 +0100

    day 2, exercise 1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn day1_1() -> i32 {
     let contents = fs::read_to_string("./input").unwrap();
     let mut xs: Vec<i32> = contents
         .split("\n\n")
-        .collect::<Vec<&str>>()
-        .into_iter()
-        .map(|e| e.split("\n").collect::<Vec<&str>>())
-        .map(|e| e.into_iter().map(|f| f.parse::<i32>().unwrap_or(0)))
+        .map(|e| e.split("\n"))
+        .map(|e| e.map(|f| f.parse::<i32>().unwrap_or(0)))
         .map(|e| e.sum())
         .collect();
     xs.sort();
@@ -19,10 +18,8 @@ fn day1_2() -> i32 {
     let contents = fs::read_to_string("./input").unwrap();
     let mut xs: Vec<i32> = contents
         .split("\n\n")
-        .collect::<Vec<&str>>()
-        .into_iter()
-        .map(|e| e.split("\n").collect::<Vec<&str>>())
-        .map(|e| e.into_iter().map(|f| f.parse::<i32>().unwrap_or(0)))
+        .map(|e| e.split("\n"))
+        .map(|e| e.map(|f| f.parse().unwrap_or(0)))
         .map(|e| e.sum())
         .collect();
     xs.sort();
@@ -30,9 +27,46 @@ fn day1_2() -> i32 {
     xs[(xs.len() - 3)..xs.len()].into_iter().sum()
 }
 
+#[macro_use]
+extern crate lazy_static;
+lazy_static! {
+    static ref SCORE_MAP: HashMap<&'static str, i32> =
+        HashMap::from([("A", 1), ("B", 2), ("C", 3), ("X", 1), ("Y", 2), ("Z", 3)]);
+}
+
+fn rock_paper_scissors(a: &str, b: &str) -> i32 {
+    const RPC_TABLE: [(i32, i32, i32); 9] = [
+        (1, 1, 3),
+        (1, 2, 6),
+        (1, 3, 0),
+        (2, 1, 0),
+        (2, 2, 3),
+        (2, 3, 6),
+        (3, 1, 6),
+        (3, 2, 0),
+        (3, 3, 3),
+    ];
+    for e in RPC_TABLE {
+        if e.0 == SCORE_MAP[a] && e.1 == SCORE_MAP[b] {
+            return e.2;
+        }
+    }
+    return 0;
+}
+
+fn day2_1() -> i32 {
+    return fs::read_to_string("./input2")
+        .unwrap()
+        .split("\n")
+        .map(|e| e.split_once(" ").unwrap())
+        .map(|e| rock_paper_scissors(e.0, e.1) + SCORE_MAP[e.1])
+        .sum();
+}
+
 fn main() {
     day1_1();
     day1_2();
+    day2_1();
 }
 
 #[cfg(test)]
@@ -45,14 +79,15 @@ mod tests {
         assert_eq!(result, 72718);
     }
 
-    // #[bench]
-    // fn bench_add_two(b: &mut Bencher) {
-    //     b.iter(|| day1_1());
-    // }
-
     #[test]
     fn test_day1_2() {
         let result = day1_2();
         assert_eq!(result, 213089);
+    }
+
+    #[test]
+    fn test_day2_1() {
+        let result = day2_1();
+        assert_eq!(result, 13268);
     }
 }
